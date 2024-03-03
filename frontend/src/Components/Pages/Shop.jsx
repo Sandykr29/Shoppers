811807d6843622bebab0b79937438c75dec1e91a
sandykr29 <sandeepkumar29774@yahoo.com>
2024-03-04 01:35:56 +0530

    basic_structure

---
+++ frontend/src/Components/Pages/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Hero } from '../Hero/Hero'
+import { Popular } from '../Popular/Popular'
+
+export const Shop = () => {
+  return (
+    <div>
+        <Hero/>
+        <Popular/>
+    </div>
+  )
+}
